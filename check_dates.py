16ce220b2b551b0e23099a061ca50dc2f43d717f
ulif <uli@gnufix.de>
2018-03-04 15:36:45 +0100

    Add main script skeleton.

---
+++ check_dates.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Check dates in package-local manpages for being up-to-date.
+"""
+
+__version__ = "0.1.dev0"
+__author__ = "ulif <ulif@gnufix.de>"
+__license__ = "GPLv3+"
+__url__ = "https://github.com/ulif/check-dates"
+
+
+#
+# Main script
+#
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
