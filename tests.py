aa53ff64748abe9f09f8737247526beca97cbafe
ulif <uli@gnufix.de>
2018-03-12 13:01:05 +0100

    Add custom failure and command runner.
    Both stolen from check-manifest.

--- tests.py
+++ tests.py
@@ -1,4 +1,9 @@
 import pytest
+from check_dates import CommandFailed
 
-def test_foo():
-    assert 1 == 1
+
+def test_command_failed(capsys):
+    # The `CommandFailed` exception gives helpful output.
+    with pytest.raises(CommandFailed) as exc:
+        raise CommandFailed('some-cmd', 23, 'foo')
+    assert str(exc.value) == 'some-cmd failed (status 23):\nfoo'
