cec66533a82c1fda5674d206a8f97dd87c03cb5f
ulif <uli@gnufix.de>
2018-03-16 12:16:57 +0100

    Add first test for `run()`.

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import pytest
-from check_dates import CommandFailed
+from check_dates import CommandFailed, run
 
 
 def test_command_failed(capsys):
@@ -7,3 +7,7 @@ def test_command_failed(capsys):
     with pytest.raises(CommandFailed) as exc:
         raise CommandFailed('some-cmd', 23, 'foo')
     assert str(exc.value) == 'some-cmd failed (status 23):\nfoo'
+
+def test_run():
+    # we can run commands in local env
+    assert run(["true"]) is ""
